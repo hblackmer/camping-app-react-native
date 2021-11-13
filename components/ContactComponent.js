9f5e8267fad5579ecb8110047016c407213da5c5
Helene <hcblackmer@gmail.com>
2021-11-13 10:12:05 -0800

    Updated Contact Component with contact information.

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
 class Contact extends Component {
 
     static navigationOptions = {
@@ -9,7 +11,16 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}
+                >
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
             </ScrollView>
         );
     }
