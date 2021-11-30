e4e67059989e0759ba5c4dbd4d47ec3b15e79d53
Helene <hcblackmer@gmail.com>
2021-11-30 12:33:48 -0800

    Used the Expo Notifications API to generate local notifications, as well as get permissions from the device to send the local notifications.

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import * as Notifications from 'expo-notifications';
 import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
@@ -48,14 +49,41 @@ class Reservation extends Component {
                         }
                     },
                     {
-                        text: "OK",
+                        text: 'OK', 
                         onPress: () => {
-                            this.resetForm()
+                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                            this.resetForm();
                         }
                     }
                 ],
             { cancelable: true }
         );
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
